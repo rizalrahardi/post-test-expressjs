25e16fbe12350bff1198aff67f8caad84ded2c26
rizalrahardi <rizalrahardi@gmail.com>
2021-02-09 18:33:19 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require ('express')
+let books = require ('./db/books.json')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/api/v1/books', (req, res) => {
+    res.status(200).json(books)
+})
+
+app.get('/api/v1/books/:id', (req, res) => {
+    const post = books.find(i => i.id === +req.params.id)
+    res.status(200).json(post)
+})
+
+
+app.post('/api/v1/books', (req, res) => {
+    console.log(req.body)
+    const { judul, sinopsis, penulis, genre} = req.body
+
+    const id = books[books.length - 1].id + 1
+    const post = {
+        id, judul, sinopsis, penulis, genre
+    }
+
+    books.push(post)
+
+    res.status(201).json(books)
+})
+
+app.delete('/api/v1/books/:id', (req, res) => {
+    books = books.filter(i => i.id !== + req.params.id)
+    res.status(200).json(books)
+})
+
+app.put('/api/v1/books/:id', (req, res) => {
+    const id = req.params.id
+    books.filter(post => {
+        if(post.id == id ){
+            post.judul = req.body.judul
+            post.sinopsis = req.body.sinopsis
+            post.penulis = req.body.penulis
+            post.genre = req.body.genre
+            return post
+        }
+    })
+    res.status(200).json(books)
+})
+
+app.listen(port, () => {
+    console.log('Server Ready')
+})
